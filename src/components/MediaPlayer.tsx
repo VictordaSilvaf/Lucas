cc136e3262d3321c3d6ffec28f34ac8b314bc66c
VictorDev <victordasilvafernandes@gmail.com>
2024-08-18 12:57:43 -0300

    feat: last changes

--- src/components/MediaPlayer.tsx
+++ src/components/MediaPlayer.tsx
@@ -34,26 +34,32 @@ const MediaPlayer: React.FC<MediaPlayerProps> = ({ my_sound, my_video }) => {
     };
 
     const videoElement = videoRef.current;
-    
+
     videoElement?.addEventListener("fullscreenchange", preventFullscreen);
     videoElement?.addEventListener("webkitbeginfullscreen", preventFullscreen); // iOS Safari
     videoElement?.addEventListener("webkitendfullscreen", preventFullscreen); // iOS Safari
 
     return () => {
       videoElement?.removeEventListener("fullscreenchange", preventFullscreen);
-      videoElement?.removeEventListener("webkitbeginfullscreen", preventFullscreen);
-      videoElement?.removeEventListener("webkitendfullscreen", preventFullscreen);
+      videoElement?.removeEventListener(
+        "webkitbeginfullscreen",
+        preventFullscreen
+      );
+      videoElement?.removeEventListener(
+        "webkitendfullscreen",
+        preventFullscreen
+      );
     };
   }, []);
 
-  const somePastDate = new Date("2023-02-01T12:00:00");
+  const somePastDate = new Date("2022-09-18T00:00:00");
 
   return (
     <div className="container mx-auto flex items-center justify-center flex-col">
-      {/* <audio ref={audioRef} autoPlay loop>
+      <audio ref={audioRef} autoPlay loop>
         <source src={my_sound} type="audio/mpeg" />
         Seu navegador não suporta o elemento de áudio.
-      </audio> */}
+      </audio>
 
       <video
         ref={videoRef}
@@ -71,15 +77,22 @@ const MediaPlayer: React.FC<MediaPlayerProps> = ({ my_sound, my_video }) => {
       <div className="mt-6 text-3xl flex flex-col gap-y-10 text-white max-w-3xl text-center">
         <TimeSince targetDate={somePastDate} />
 
+        <div className="">❤️</div>
+
+        <hr className="w-1/2 mx-auto" />
+
         {step === 0 && (
           <div className="">
-            <h1 className="text-red-500">Você...</h1>
+            <h1 className="text-red-500 text-3xl">
+              Só pra você lembrar toda vez que acessar isso:
+            </h1>
 
             <div className="w-full flex flex-col">
-              <p className="mt-4">
-                11111111 Dolorum hic veniam minima hic veniam minima hic veniam
-                minima hic veniam minima hic veniam minima debitis rem nihil!
-                Deleniti, quibusdam. Corrupti, reiciendis.
+              <p className="mt-4 text-lg">
+                Hoje comemoramos 1 ano e 11 meses de uma jornada incrível
+                juntos. Cada momento ao seu lado tem sido uma alegria e uma
+                descoberta constante, tenho aprendido com você como realmente
+                funciona um amor de verdade.
               </p>
             </div>
           </div>
@@ -87,13 +100,17 @@ const MediaPlayer: React.FC<MediaPlayerProps> = ({ my_sound, my_video }) => {
 
         {step === 1 && (
           <div className="">
-            <h1 className="text-red-500">é...</h1>
+            <h1 className="text-red-500 text-3xl">
+              Só pra você lembrar toda vez que acessar isso:
+            </h1>
 
             <div className="w-full flex flex-col">
-              <p className="mt-4">
-                222222222 Dolorum hic veniam minima hic veniam minima hic veniam
-                minima hic veniam minima hic veniam minima debitis rem nihil!
-                Deleniti, quibusdam. Corrupti, reiciendis.
+              <p className="mt-4 text-lg">
+                Sua presença ilumina meus dias e seu amor torna tudo mais
+                especial. Sou grato por cada risada, cada conversa e cada
+                experiência que compartilhamos. Deus foi muito bom comigo em ter
+                guardado você para mim, pq sem você, nao sei o que seria de mim!
+                ❤️
               </p>
             </div>
           </div>
@@ -101,78 +118,22 @@ const MediaPlayer: React.FC<MediaPlayerProps> = ({ my_sound, my_video }) => {
 
         {step === 2 && (
           <div className="">
-            <h1 className="text-red-500">a...</h1>
+            <h1 className="text-red-500 text-3xl">Te amo!</h1>
 
             <div className="w-full flex flex-col">
-              <p className="mt-4">
-                33333333 Dolorum hic veniam minima hic veniam minima hic veniam
-                minima hic veniam minima hic veniam minima debitis rem nihil!
-                Deleniti, quibusdam. Corrupti, reiciendis.
+              <p className="mt-4 text-lg">
+                E te amarei até a volta Dele! Mesmo se um dia seu amor por mim
+                acabar, saiba que eu fui realmente feliz com você! Sempre serei
+                grato pela sua vida
               </p>
             </div>
           </div>
         )}
 
-        {step === 3 && (
-          <div className="">
-            <h1 className="text-red-500">mulher...</h1>
-
-            <div className="w-full flex flex-col">
-              <p className="mt-4">
-                444444444 Dolorum hic veniam minima hic veniam minima hic veniam
-                minima hic veniam minima hic veniam minima debitis rem nihil!
-                Deleniti, quibusdam. Corrupti, reiciendis.
-              </p>
-            </div>
-          </div>
-        )}
-
-        {step === 4 && (
-          <div className="">
-            <h1 className="text-red-500">da...</h1>
-
-            <div className="w-full flex flex-col">
-              <p className="mt-4">
-                555555555 Dolorum hic veniam minima hic veniam minima hic veniam
-                minima hic veniam minima hic veniam minima debitis rem nihil!
-                Deleniti, quibusdam. Corrupti, reiciendis.
-              </p>
-            </div>
-          </div>
-        )}
-
-        {step === 5 && (
-          <div className="">
-            <h1 className="text-red-500">minha...</h1>
-
-            <div className="w-full flex flex-col">
-              <p className="mt-4">
-                666666666 Dolorum hic veniam minima hic veniam minima hic veniam
-                minima hic veniam minima hic veniam minima debitis rem nihil!
-                Deleniti, quibusdam. Corrupti, reiciendis.
-              </p>
-            </div>
-          </div>
-        )}
-
-        {step === 6 && (
-          <div className="">
-            <h1 className="text-red-500">vida!</h1>
-
-            <div className="w-full flex flex-col">
-              <p className="mt-4">
-                7777777777 Dolorum hic veniam minima hic veniam minima hic
-                veniam minima hic veniam minima hic veniam minima debitis rem
-                nihil! Deleniti, quibusdam. Corrupti, reiciendis.
-              </p>
-            </div>
-          </div>
-        )}
-
-        {step < 6 && (
+        {step < 2 && (
           <button
             onClick={() => setStep(step + 1)}
-            className="text-white bg-red-500 px-6 py-2 rounded-lg hover:bg-red-700 duration-300 ease-in-out text-caveat text-xl"
+            className="text-white bg-red-500 px-6 py-2 rounded-lg hover:bg-red-700 duration-300 ease-in-out text-xl"
           >
             Continuar
           </button>
